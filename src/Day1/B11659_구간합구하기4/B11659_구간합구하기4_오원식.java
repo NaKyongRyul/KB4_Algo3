b296573c3f0528a8632ce48cbc7dd92b50096378
오원식 <omsoms777@naver.com>
2023-07-10 16:37:39 +0900

    B2018_수들의합5_오원식

--- src/Day1/B11659_구간합구하기4/B11659_구간합구하기4_오원식.java
+++ src/Day1/B11659_구간합구하기4/B11659_구간합구하기4_오원식.java
@@ -14,16 +14,16 @@ public class B11659_구간합구하기4_오원식 {
 		int m = Integer.parseInt(input[1]);
 		
 		String [] arr = reader.readLine().split(" ");
-		int [] arr2 = new int[n];
+		//int [] arr2 = new int[n];
 		
-		for(int i = 0; i < arr.length; i++) {
-			arr2[i] = Integer.parseInt(arr[i]);
-		}
+//		for(int i = 0; i < arr.length; i++) {
+//			arr2[i] = Integer.parseInt(arr[i]);
+//		}
 		
 		int [] dp = new int[n+1];
 		
-		for(int i = 1; i<arr2.length+1; i++) {
-			dp[i] = dp[i-1] + arr2[i-1];
+		for(int i = 1; i<arr.length+1; i++) {
+			dp[i] = dp[i-1] + Integer.parseInt(arr[i-1]);
 		}
 		
 		for(int i = 0; i<m; i++) {
