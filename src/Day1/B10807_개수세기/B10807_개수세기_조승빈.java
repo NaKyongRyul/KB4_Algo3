fee183dce578669fb5454f864b98cea7fcb52b77
BullChallenger <am23am45@naver.com>
2023-07-10 14:22:39 +0900

    B10807_개수세기_조승빈

---
+++ src/Day1/B10807_개수세기/B10807_개수세기_조승빈.java
@@ -0,0 +1,26 @@
+package Day1.B10807_개수세기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10807_개수세기_조승빈 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int cnt = 0;
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int target = Integer.parseInt(br.readLine());
+
+		while(st.hasMoreTokens()) {
+			if (target == Integer.parseInt(st.nextToken())) {
+				cnt++;
+			}
+		}
+
+		System.out.println(cnt);
+	}
+}
