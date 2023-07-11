5e4d576a717521fd5a8007709bca011c31438027
yebin <dev.devin.kr@gmail.com>
2023-07-11 15:43:38 +0900

    B17608_막대기_최예빈

---
+++ src/Day2/B17608_막대기/B17609_막대기_최예빈.java
@@ -0,0 +1,37 @@
+package com.itskb.codingtest.day07;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.Stack;
+
+public class B17609_막대기_최예빈 {
+
+    static int N;
+    static int highest, temp, count;
+    static Stack<Integer> h;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        h = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            h.push(Integer.parseInt(br.readLine()));
+        }
+
+        highest = h.peek();
+        count++;
+
+        while (!h.isEmpty()) {
+            temp = h.pop();
+            if (highest < temp) {
+                highest = temp;
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
