d81d30c3ffa895e0ab90c3657488e5bce518ff4d
WoongKi1115 <nwk6638@gmail.com>
2023-07-11 15:36:50 +0900

    B17608_막대기_나웅기

---
+++ src/Day2/B17608_막대기/B17608_막대기_나웅기.java
@@ -0,0 +1,27 @@
+package Day2.B17608_막대기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class B17608_막대기_나웅기 {
+	public static void main(String[] args) throws IOException{
+		Stack<Integer> stack = new Stack<>();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(reader.readLine());
+		for (int i = 0; i < N; i++) {
+			stack.push(Integer.parseInt(reader.readLine()));
+		}
+		int result = 1;
+		int top = stack.peek();
+		while (!stack.isEmpty()) {
+			int tall = stack.pop();
+			if (tall > top) {
+				top = tall;
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
