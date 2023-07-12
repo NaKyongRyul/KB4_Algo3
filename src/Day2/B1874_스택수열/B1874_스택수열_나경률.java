e531fd6775c262f2bcf31fc2c8842d4728860487
Kyongryul <skrudfbf0813@naver.com>
2023-07-12 20:22:53 +0900

    B1874_스택수열_나경률

--- src/Day2/B1874_스택수열/B1874_스택수열_나경률.java
+++ src/Day2/B1874_스택수열/B1874_스택수열_나경률.java
@@ -1,9 +1,49 @@
 package Day2.B1874_스택수열;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class B1874_스택수열_나경률 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+				
+		int N = Integer.parseInt(br.readLine());
 		
+		int[] arr = new int[N];
+		Stack <Integer> stack = new Stack<>();
+
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int index = 1;
+		
+		for(int i=0; i<N; i++) {
+			int now = arr[i];
+			
+			if(stack.isEmpty() || now > stack.peek()) {
+				for(int j=index; j<=now; j++) {
+					stack.push(j);
+					sb.append("+" + "\n");
+					index++;
+				}
+				stack.pop();
+				sb.append("-" + "\n");
+			}
+			else if(now == stack.peek()) {
+				stack.pop();
+				sb.append("-" + "\n");
+			}
+			else {
+				System.out.println("NO");
+				return;
+			}
+		}
+		System.out.println(sb);
 	}
 
 }
