84f52bb32075f751f717b0638979afee4ac3bda8
gitJaen <dksehgis@androidjava.com>
2023-07-12 15:34:40 +0900

    add B2750_버블정렬

---
+++ src/Day3/B2750_버블정렬/B2750_버블정렬_Sol.java
@@ -0,0 +1,16 @@
+package Day3.B2750_버블정렬;
+
+public class B2750_버블정렬_Sol {
+
+	
+	public static void swap(int [] arr , int i , int j) {
+		int temp = arr[i]
+	}
+	
+	public static void main(String[] args) {
+		
+		int [] arr = {1,2,3,4};
+		
+		
+	}
+}
