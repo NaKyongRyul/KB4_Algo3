09497910971404257a612aaf17f7b1153e7a6321
WoongKi1115 <nwk6638@gmail.com>
2023-07-12 10:48:50 +0900

    B1158_요세푸스문제_나웅기

--- src/Day2/B1874_스택수열/B1874_스택수열_나웅기.java
+++ src/Day2/B1874_스택수열/B1874_스택수열_나웅기.java
@@ -1,5 +1,43 @@
 package Day2.B1874_스택수열;
 
-public class B1874_스택수열_나웅기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
 
+public class B1874_스택수열_나웅기 {
+	public static void main(String[] args) throws IOException{
+			
+			Stack<Integer> stack = new Stack<>();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			int n = Integer.parseInt(reader.readLine());
+			int [] arr = new int [n];
+			for (int i = 0; i<n; i++) {
+				arr[i] = Integer.parseInt(reader.readLine());
+			}
+			ArrayList<String> result = new ArrayList<>();
+			int idx = 0;
+	
+			for (int i = 1; i<n+1; i++) {
+				stack.add(i);
+				result.add("+");
+				while (!stack.isEmpty() && arr[idx]==stack.peek()) {
+					stack.pop();
+					result.add("-");
+					if (idx<n-1) idx++;
+	
+				}
+			}
+			if (stack.isEmpty()) {
+				for (String i : result) {
+					System.out.println(i);
+				}
+			} else {
+				System.out.println("NO");
+			}
+			
+	
+	
+	}
 }
---
+++ src/Day3/B1158_요세푸스문제/B1158_요세푸스문제_나웅기.java
@@ -0,0 +1,5 @@
+package Day3.B1158_요세푸스문제;
+
+public class B1158_요세푸스문제_나웅기 {
+
+}
