503f0859660b2ad62b49b1dd5929e7f2e4cca133
ulh1121 <ulh1121@gmail.com>
2023-07-14 00:00:49 +0900

    B11659_구간합구하기4_황지현

--- src/Day1/B11659_구간합구하기4/B11659_구간합구하기4_황지현.java
+++ src/Day1/B11659_구간합구하기4/B11659_구간합구하기4_황지현.java
@@ -1,5 +1,23 @@
 package Day1.B11659_구간합구하기4;
 
-public class B11659_구간합구하기4_황지현 {
+import java.util.Scanner;
 
+public class B11659_구간합구하기4_황지현 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int [] arr = new int[n+1];
+		for(int i=1;i<=n;i++) {
+			arr[i]=arr[i-1]+sc.nextInt();
+	
+		}
+		
+		for(int j=0;j<m;j++) {
+			int start=sc.nextInt();
+			int end=sc.nextInt();
+			System.out.println(arr[end]-arr[start-1]);
+			
+		}
+	}
 }
