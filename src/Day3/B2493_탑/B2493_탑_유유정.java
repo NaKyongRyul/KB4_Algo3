d00afd0ae9d9e1241a582cb3345bc975a47423ea
unknown <imuu30071126@gmail.com>
2023-07-13 14:37:05 +0900

    B2750_병합정렬_유유정

--- src/Day3/B2493_탑/B2493_탑_유유정.java
+++ src/Day3/B2493_탑/B2493_탑_유유정.java
@@ -10,18 +10,42 @@ import java.util.Stack;
 public class B2493_탑_유유정 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		Stack<Integer> stack01 = new Stack<>();
+		Stack<Integer> stack = new Stack<>();
 		int N = Integer.parseInt( bf.readLine());
 		
 		String s = bf.readLine();
 		String []str = s.split(" ");
 		int [] arr = new int [N];
+		int [] answer = new int [N];
 		for(int i=0; i<N;i++) {
 			arr[i] = (Integer.parseInt(str[i]));			
 		}
 		for(int i=0;i<N;i++) {
+			int now =arr[i];
+			if(stack.isEmpty()) {
+				stack.add(i);
+				answer[i]=0;
+				System.out.println(answer[i]);
+				continue;
+			}
+			while(!stack.isEmpty() && arr[stack.peek()]<now) {
+				stack.pop();
+				
+			}
+			
+			if(!stack.isEmpty()) {
+				answer[i] = stack.peek()+1;
+				stack.push(i);
+					
+			}
+				
 			
 		}
+		
+		for(int i=0;i<N-1;i++) {
+			System.out.print(answer[i]+ " ");
+		}
+		System.out.println(answer[N-1]);
 	}
 
 }
