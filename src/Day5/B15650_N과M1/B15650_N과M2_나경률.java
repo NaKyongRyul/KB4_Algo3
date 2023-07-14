8c4e3775555a3d8d6effd6fb13e1fb96dcd9d213
Kyongryul <skrudfbf0813@naver.com>
2023-07-14 12:43:14 +0900

    B15650_N과M2_나경률

---
+++ src/Day5/B15650_N과M1/B15650_N과M2_나경률.java
@@ -0,0 +1,40 @@
+package Day5.B15650_N과M1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15650_N과M2_나경률 {
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] selected;
+	
+	public static void rec_func(int k) {
+		if(k == M+1) {
+			for(int i=1; i<=M; i++) {
+				sb.append(selected[i] + " ");
+			}
+			sb.append("\n");
+		} else {
+			for(int cand=selected[k-1]+1; cand<=N; cand++) {
+				selected[k] = cand;
+				rec_func(k+1);
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		selected = new int[M+1];
+		
+		rec_func(1);
+		System.out.println(sb);
+	}
+}
