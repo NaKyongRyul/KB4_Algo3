013cd0e4066e5a0b9a2b58915ae93120ab59fd7c
jaeeunchang <cjeun8@naver.com>
2023-07-17 11:24:18 +0900

    P_크레인인형뽑기_장재은

---
+++ src/Day6/P_크레인인형뽑기/P_크레인인형뽑기_장재은.java
@@ -0,0 +1,27 @@
+package Day6.P_크레인인형뽑기;
+
+import java.util.Stack;
+public class P_크레인인형뽑기_장재은 {
+	public int solution(int[][] board, int[] moves) {
+        Stack <Integer> stack = new Stack<>();
+        int answer = 0;
+        int num = 0;
+        for(int m:moves){
+            for(int i=0; i<board.length; i++){
+                if(board[i][m-1] != 0) {
+                    num = board[i][m-1];
+                    board[i][m-1] = 0;
+                    if(!stack.isEmpty() && stack.peek() == num){
+                        stack.pop();
+                        answer += 2;   
+                    }
+                    else{
+                        stack.push(num);
+                    }                    
+                    break;
+                }       
+            }
+        }
+        return answer;
+    }
+}
