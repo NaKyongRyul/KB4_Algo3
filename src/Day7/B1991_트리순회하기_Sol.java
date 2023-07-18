9dcb53072876b497b99403c2908178166f18f830
gitJaen <dksehgis@androidjava.com>
2023-07-18 10:33:19 +0900

    add B1991_트리순회

---
+++ src/Day7/B1991_트리순회하기_Sol.java
@@ -0,0 +1,15 @@
+package Day7;
+
+public class B1991_트리순회하기_Sol {
+
+	public static void main(String[] args) {
+		
+		
+		char c = 'C';
+		char d = 'D';
+		
+		System.out.println( (int) ('A' - 65) );
+
+		
+	}
+}
