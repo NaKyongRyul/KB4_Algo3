a0cb7654d122a92d71895de28db2db2c7eec367e
jiwon <jiwonno12345@gmail.com>
2023-07-19 17:45:20 +0900

    B9663_N_Queen_정지원

---
+++ src/Day8/B9663_N-Queen/B9663_N_Queen_정지원.java
@@ -0,0 +1,62 @@
+package Day8.B9663_N-Queen;
+
+import java.util.Scanner;
+
+public class B9663_N_Queen_정지원 {
+	static int n;
+	static int[][] board;
+	static int count = 0;
+	
+	static boolean isSafe(int y, int x){
+		
+		for(int i=0;i<y;i++) {
+			if(board[i][x]==1) {
+				return false;
+			}h
+			else if((x-(i+1))>=0 && board[y-i-1][x-(i+1)]==1) {
+				return false;
+			}
+			else if(x+i+1<n && board[y-i-1][x+i+1]==1) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	static void func(int line, int col) {
+		
+		if(line==n-1) {
+			count++;
+			return;
+		}
+		
+		board[line][col] = 1;
+		
+		for(int i=0;i<n;i++) {
+			if(isSafe(line+1, i)) func(line+1, i);
+			else {
+				continue;
+			}
+		}
+		
+		
+		board[line][col] = 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n= sc.nextInt();
+		board = new int[n][n];
+		
+		
+		for(int i=0;i<n;i++) {
+			func(0, i);
+			
+		}
+		
+		System.out.println(count);
+	}
+}
