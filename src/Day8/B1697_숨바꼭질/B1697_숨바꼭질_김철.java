ef7ef45a6eadf97ca7ec056b41a9936bd486e23a
ddongman <cheol12>
2023-07-19 13:22:59 +0900

    B1697_숨바꼭질_김철

---
+++ src/Day8/B1697_숨바꼭질/B1697_숨바꼭질_김철.java
@@ -0,0 +1,50 @@
+package Day8.B1697_숨바꼭질;
+
+import java.io.*;
+import java.util.*;
+
+class B1697_숨바꼭질_김철{
+    static int[] arr;
+    static int N,K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer token;
+
+        token = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(token.nextToken());
+        K = Integer.parseInt(token.nextToken());
+
+        arr = new int[100001];
+
+        BFS(N);
+
+        bw.write(arr[K] + "");
+        bw.close();
+    }
+
+    static void BFS(int x){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(x);
+        arr[x] = 0;
+
+        while(!q.isEmpty()){
+            int now = q.poll();
+            if(now == K) return;
+
+            if(now+1 <= 100000 && arr[now+1] == 0){
+                arr[now+1] = arr[now] + 1;
+                q.offer(now+1);
+            }
+            if(now-1 >= 0 && arr[now-1] == 0){
+                arr[now-1] = arr[now] + 1;
+                q.offer(now-1);
+            }
+            if(now*2 <= 100000 && arr[now*2] == 0){
+                arr[now*2] = arr[now] + 1;
+                q.offer(now*2);
+            }
+        }
+    }
+}
