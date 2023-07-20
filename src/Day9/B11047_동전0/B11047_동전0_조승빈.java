c31b6c3d4ceb75b1cda37d21b53fcc7e959d4669
BullChallenger <am23am45@naver.com>
2023-07-20 11:18:26 +0900

    B11047_동전0_조승빈

---
+++ src/Day9/B11047_동전0/B11047_동전0_조승빈.java
@@ -0,0 +1,32 @@
+package Day9.B11047_동전0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11047_동전0_조승빈 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        int[] coins = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (K >= coins[i]) {
+                cnt += K / coins[i];
+                K %= coins[i];
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
