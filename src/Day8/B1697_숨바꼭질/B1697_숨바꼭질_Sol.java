de16b119a37aea5d611b49fcbffe98fa73db5c50
gitJaen <126427664+gitJaen@users.noreply.github.com>
2023-07-20 09:43:50 +0900

    Update B1697_숨바꼭질_Sol.java

--- src/Day8/B1697_숨바꼭질/B1697_숨바꼭질_Sol.java
+++ src/Day8/B1697_숨바꼭질/B1697_숨바꼭질_Sol.java
@@ -1,6 +1,7 @@
 package Day8.B1697_숨바꼭질;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -11,32 +12,60 @@ public class B1697_숨바꼭질_Sol {
 	static int maxValue = 100001;
 	static int ans = 0;
 	static int [] time;
-	
-	
+
+
+	// time 배열 초기값 = -1 (정답)
 	public static void searchBFS() {
-		
+
 		Queue<Integer> que = new ArrayDeque<>();
 		time = new int [maxValue];
-		
+		Arrays.fill(time , -1);
+
 		que.add(N);
 		time[N] = 0;
-		
+
 		while(!que.isEmpty()) {
-			
+//			System.out.println(Arrays.toString(Arrays.copyOf(time, 10)));
 			int now = que.poll();
 			if(now == K) return;
-			
+
+			// 3). X*2 연산
+			if( (now*2) >= 0 && (now*2) < maxValue && time[now * 2] == -1) {
+				que.add(now*2);
+
+				time[now*2] = time[now] + 1;
+			}
+
 			// 1). X + 1 
-			if( (now + 1) >= 0 && (now + 1) < maxValue && time[now + 1] == 0 ) {
+			if( (now + 1) >= 0 && (now + 1) < maxValue && time[now + 1] == -1 ) {
 				que.add(now + 1);
 				time[now + 1] = time[now] + 1;
 			}
-			
+
 			// 2). X - 1
-			if( (now - 1) >= 0 && (now - 1) < maxValue && time[now - 1] == 0) {
+			if( (now - 1) >= 0 && (now - 1) < maxValue && time[now - 1] == -1) {
 				que.add(now - 1);
 				time[now - 1] = time[now] + 1;
 			}
+
+
+		}
+	}
+
+
+	// time 배열 초기값 = 0 인 경우 (오답)
+	public static void searchBFS2() {
+
+		Queue<Integer> que = new ArrayDeque<>();
+		time = new int [maxValue];
+
+		que.add(N);
+		time[N] = 0;
+
+		while(!que.isEmpty()) {
+//			System.out.println(Arrays.toString(Arrays.copyOf(time, 10)));
+			int now = que.poll();
+			if(now == K) return;
 			
 			// 3). X*2 연산
 			if( (now*2) >= 0 && (now*2) < maxValue && time[now * 2] == 0) {
@@ -44,19 +73,31 @@ public class B1697_숨바꼭질_Sol {
 				time[now*2] = time[now] + 1;
 			}
 			
+			// 1). X + 1 
+			if( (now + 1) >= 0 && (now + 1) < maxValue && time[now + 1] == 0 ) {
+				que.add(now + 1);
+				time[now + 1] = time[now] + 1;
+			}
+
+			// 2). X - 1
+			if( (now - 1) >= 0 && (now - 1) < maxValue && time[now - 1] == 0) {
+				que.add(now - 1);
+				time[now - 1] = time[now] + 1;
+			}
+
 		}
 	}
 
-	
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		K = sc.nextInt();
-		
+
 		searchBFS();
+		
 		System.out.println(time[K]);
 
 	}
-
 }
