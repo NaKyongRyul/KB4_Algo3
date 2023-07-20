92c001947dd6c0868ef80f5e2b8d902031361a10
hwi215 <buc06137@naver.com>
2023-07-20 17:32:51 +0900

    B1920_수찾기_김휘경

--- src/Day9/B1931_회의실배정/B1931_회의실배정_김휘경.java
+++ src/Day9/B1931_회의실배정/B1931_회의실배정_김휘경.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
- 
+
 
 public class B1931_회의실배정_김휘경 {
     public static void main(String[] args) throws IOException {
